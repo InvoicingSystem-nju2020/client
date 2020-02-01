d74f4ea3376249920f8badad4becea0a01301158
161250181 <498690270@qq.com>
2020-02-02 00:34:15 +0800

    add 环境配置

--- src/util/config.ts
+++ src/util/config.ts
@@ -1,10 +1,15 @@
-console.log("PATH_NAME: "+document.location.pathname);
+// console.log(process.env);
+// console.log(process.env.REACT_APP_BASE_URL);
 
-const BASE_URL:string = '/';
+// const BASE_URL:string = '/';
+//
+// const API_URL:string = process.env.NODE_ENV === 'development' ?
+//   '' :  // 生产
+//   '';   // 产品
 
-const API_URL:string = process.env.NODE_env === 'development' ?
-  '' :  // 生产
-  '';   // 产品
+const BASE_URL:any = process.env.REACT_APP_BASE_URL;
+
+const API_URL:any = process.env.REACT_APP_API_URL;
 
 export const BaseParam = {
   BASE_URL: BASE_URL,
