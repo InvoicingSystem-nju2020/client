cc060ffc6914f0acc13d84e5d5e449d55573936a
161250181 <498690270@qq.com>
2020-05-15 21:32:34 +0800

    add 供应商列表的搜索筛选

--- src/containers/suppliers/list/SuppliersList.tsx
+++ src/containers/suppliers/list/SuppliersList.tsx
@@ -1,11 +1,12 @@
 import React, {useEffect, useState} from 'react';
 
-import { Form, Row, Col, PageHeader, Input, Button } from 'antd';
+import {Form, Row, Col, PageHeader, Input, Button, notification} from 'antd';
 import { Table } from 'antd';
 import { EditOutlined, PlusOutlined } from '@ant-design/icons';
 
 import {AdvancedSearchForm} from "../../../components/advanced-search-form/AdvancedSearchForm";
 import {Link} from "react-router-dom";
+import {getSuppliers, GetSuppliersParams} from "../../../api/SupplierApi";
 
 // 表格列
 const { Column } = Table;
@@ -89,17 +90,31 @@ const conditions = [
 function SuppliersList() {
   let [data, setData] = useState<SupplierInfo[]>([]) ;  // dataSource数组
   let [loading, setLoading] = useState(true);
+  const [params, setParams] = useState<GetSuppliersParams>({});  // 搜索筛选参数
 
   let pageSize: number = 20;
 
   // 获取商品列表
   function getSuppliersList() {
-    let temp:SupplierInfo[] = [
-      new SupplierInfo('LINING', '李宁（北京）体育用品有限公司', '18000000000', '一般合作商', '运动鞋服、体育器材', '羽球系列', '李宁', '陈叶', '男', '区域经理', 'xxx@a.com')
-    ];
-    setData(temp);
-    setLoading(false);
-  }
+    // let temp:SupplierInfo[] = [
+    //   new SupplierInfo('LINING', '李宁（北京）体育用品有限公司', '18000000000', '一般合作商', '运动鞋服、体育器材', '羽球系列', '李宁', '陈叶', '男', '区域经理', 'xxx@a.com')
+    // ];
+    // setData(temp);
+    // setLoading(false);
+    let api = getSuppliers(params);
+    setLoading(true);
+    api.then(response => {
+      console.log(response);
+      let list = response.data.suppliersList;
+      setData(list);
+    }).catch(reason => {
+      console.error(reason);
+      notification.error({message: '发生了错误', description: reason.toString()});
+    }).finally(() => {
+      setLoading(false);
+    });
+  };
+
   // 加载时获取一次商品列表
   useEffect(() => {
     getSuppliersList();
@@ -107,8 +122,33 @@ function SuppliersList() {
 
   // 处理表格变化
   function handleTableChange (pagination:any, filters:any, sorter:any) {
+    console.log(filters);
+    console.log(sorter);
+    // 性别筛选
+    if(filters.sex){
+      if(filters.sex.length === 1){
+        params.sex = filters.sex[0] === '男' ? 1 : 2;
+      }else{  // 男和女，即全部
+        params.sex = 0;
+      }
+    }
+    // 排序
+    params.sorter = sorter.field;
+    params.desc = sorter.order === 'descend' ? 1 : 0;
+    console.log(params);
+    // 获取列表
+    getSuppliersList();
+  };
 
-  }
+  // 处理搜索栏
+  function onSearchFormFinish(name: string, info: any) {
+    console.log(info);
+    // 组装数据
+    Object.assign(params, info.values);
+    console.log("params", params);
+    // 获取列表
+    getSuppliersList();
+  };
 
   return(
     <div>
@@ -125,13 +165,15 @@ function SuppliersList() {
       </PageHeader>
       <div className={"ContentContainer"}>
         <div>
-          <AdvancedSearchForm conditions={conditions}/>
+          <Form.Provider onFormFinish={onSearchFormFinish}>
+            <AdvancedSearchForm conditions={conditions}/>
+          </Form.Provider>
         </div>
-        <Table dataSource={data} rowKey={'suppliersNumber'} pagination={{ pageSize: pageSize }} loading={loading}
+        <Table dataSource={data} rowKey={'supplierNumber'} pagination={{ pageSize: pageSize }} loading={loading}
                onChange={handleTableChange}
         >
-          <Column title={"供应商编号"} dataIndex={"suppliersNumber"} sorter={true}/>
-          <Column title={"名称"} dataIndex={"suppliersName"} sorter={true}/>
+          <Column title={"供应商编号"} dataIndex={"supplierNumber"} sorter={true}/>
+          <Column title={"名称"} dataIndex={"supplierName"} sorter={true}/>
           <Column title={"生产类别"} dataIndex={"productionCategories"} sorter={true} />
           <Column title={"采购类别"} dataIndex={"purchasingCategories"}/>
           <Column title={"法人"} dataIndex={"legalPerson"}/>
